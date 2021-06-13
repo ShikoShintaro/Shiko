451cab62b8d7d3735631db65943c9f9e7b89f6a8
Shiko Shintaro <shikoshintaro21@gmail.com>
2021-06-13 23:28:39 +0800

    okay

--- commands/rank/leaderboard.js
+++ commands/rank/leaderboard.js
@@ -9,13 +9,18 @@ module.exports = {
     aliases: ['lb'],
 
     run: async (client, message, args, shikoDB) => {
-        const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10); 
+        const roleColor =
+            message.guild.me.displayHexColor === "#000000"
+                ? "#ffffff"
+                : message.guild.me.displayHexColor;
+
+        const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10);
 
         if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
 
-        const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard, true); 
+        const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard, true);
 
-        const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`); 
+        const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
 
         const Leaderboard = new MessageEmbed()
             .setColor(roleColor)
