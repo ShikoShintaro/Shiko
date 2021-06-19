ef7166c942261c6d234efb46b70a4d41039da620
Shiko Shintaro <shikoshintaro21@gmail.com>
2021-06-19 10:19:21 +0800

    updates

--- events/ready.js
+++ events/ready.js
@@ -5,14 +5,31 @@ const config = require('../config/shiko.json')
 
 client.on('ready', () => {
     console.log('Im ready Mastah');
+
+    const activity_list = [
+        ["With my master~", "PLAYING"],
+        ["My Master's actions", "WATCHING"],
+        ["s!help", "LISTENING"],
+        ["Manager", "WATCHNG"],
+        ["With Manager", "PLAYING"],
+        ["My beloved one", "WATCHING"],
+        ["Neko world", "WATCHING"],
+        ["Earth", "WATCHING"],
+        ["in Shadow Realm", "COMPETING"]
+    ];
+    setInterval(() => {
+
+		const index = Math.floor(Math.random() * activity_list.length);
+		client.user.setActivity(activity_list[index][0], {type: activity_list[index][1]});
+	}, 10000);
 })
 
 let shikouri = config.shikodb
 
 let shikodb;
 let shkodb = new mongodb.MongoClient(shikouri, {
-    useNewUrlParser : true,
-    useUnifiedTopology : true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 shkodb.connect((err, db) => {
